3aad3b0832bcf906e4c74278f0754b9801c1b436
Adamson Vispo <eydamson.dev@gmail.com>
2025-03-22 03:36:21 +0800

    Add space

--- apps/frontend/src/components/Hero.tsx
+++ apps/frontend/src/components/Hero.tsx
@@ -1,6 +1,5 @@
 import { Button } from "./ui/button";
 import { buttonVariants } from "./ui/button";
-import { HeroCards } from "./HeroCards";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import { HeroMarquee } from "./HeroMarquee";
 
@@ -9,7 +8,7 @@ export const Hero = () => {
     <section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
       <div className="text-center lg:text-start space-y-6">
         <main className="text-5xl md:text-6xl font-bold">
-          Dito sa
+          Dito sa{" "}
           <h1 className="inline">
             <span className="inline bg-gradient-to-r from-[#F596D3]  to-[#D247BF] text-transparent bg-clip-text">
               CJCRSG
