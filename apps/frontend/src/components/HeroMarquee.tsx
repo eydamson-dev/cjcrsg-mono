e43b4ce6630f66ada6f745dd6011162a9038bf16
Adamson Vispo <eydamson.dev@gmail.com>
2025-03-22 12:01:16 +0800

    Use img in marquee

--- apps/frontend/src/components/HeroMarquee.tsx
+++ apps/frontend/src/components/HeroMarquee.tsx
@@ -1,24 +1,24 @@
-import { cn } from "@/utilities";
-import { Marquee } from "@/components/magicui/marquee";
+import { cn } from '@/utilities';
+import { Marquee } from '@/components/magicui/marquee';
 
 const reviews = [
   {
-    name: "Jack",
-    username: "@jack",
+    name: 'Jack',
+    username: '@jack',
     body: "I've never seen anything like this before. It's amazing. I love it.",
-    img: "https://avatar.vercel.sh/jack",
+    img: 'https://avatar.vercel.sh/jack',
   },
   {
-    name: "Jill",
-    username: "@jill",
+    name: 'Jill',
+    username: '@jill',
     body: "I don't know what to say. I'm speechless. This is amazing.",
-    img: "https://avatar.vercel.sh/jill",
+    img: 'https://avatar.vercel.sh/jill',
   },
   {
-    name: "John",
-    username: "@john",
+    name: 'John',
+    username: '@john',
     body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/john",
+    img: 'https://avatar.vercel.sh/john',
   },
 ];
 
@@ -39,19 +39,17 @@ const ReviewCard = ({
   return (
     <figure
       className={cn(
-        "relative h-full w-36 cursor-pointer overflow-hidden rounded-xl border p-4",
+        'relative h-full w-36 cursor-pointer overflow-hidden rounded-xl border p-4',
         // light styles
-        "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
+        'border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]',
         // dark styles
-        "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
+        'dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]'
       )}
     >
       <div className="flex flex-row items-center gap-2">
         <img className="rounded-full" width="32" height="32" alt="" src={img} />
         <div className="flex flex-col">
-          <figcaption className="text-sm font-medium dark:text-white">
-            {name}
-          </figcaption>
+          <figcaption className="text-sm font-medium dark:text-white">{name}</figcaption>
           <p className="text-xs font-medium dark:text-white/40">{username}</p>
         </div>
       </div>
@@ -64,18 +62,23 @@ export function HeroMarquee() {
   return (
     <div className="relative flex h-[500px] w-full flex-row items-center justify-center overflow-hidden">
       <Marquee pauseOnHover vertical className="[--duration:20s]">
-        {firstRow.map((review) => (
+        {firstRow.map(review => (
           <ReviewCard key={review.username} {...review} />
         ))}
       </Marquee>
       <Marquee reverse pauseOnHover vertical className="[--duration:20s]">
-        {secondRow.map((review) => (
+        {secondRow.map(review => (
           <ReviewCard key={review.username} {...review} />
         ))}
       </Marquee>
       <Marquee pauseOnHover vertical className="[--duration:20s]">
-        {firstRow.map((review) => (
-          <ReviewCard key={review.username} {...review} />
+        {firstRow.map(review => (
+          <img
+            key={review.username}
+            className="rounded-xl object-cover w-56 h-56"
+            src="/website-template-OG.webp"
+            alt={review.username}
+          />
         ))}
       </Marquee>
       <div className="pointer-events-none absolute inset-x-0 top-0 h-1/4 bg-gradient-to-b from-background"></div>
@@ -83,4 +86,3 @@ export function HeroMarquee() {
     </div>
   );
 }
-
