d8a8fc3a909d75f700434f11f77724cb17ddf0bf
Adamson Vispo <eydamson.dev@gmail.com>
2025-06-12 03:13:40 +0800

    style: redesign the login page

--- apps/frontend/src/components/login-form/index.tsx
+++ apps/frontend/src/components/login-form/index.tsx
@@ -1,6 +1,9 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import Link from "next/link"
+import { ArrowLeft } from "lucide-react";
 import { Input } from "@/components/ui/input"
 import { useToast } from "@/hooks/use-toast"
 import { cn } from "@/lib/utils"
@@ -42,44 +45,100 @@ export function LoginForm({
 
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>
-                  Email
-                </FormLabel>
-                <FormControl>
-                  <Input placeholder="Enter your email" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          >
-          </FormField>
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>
-                  Password
-                </FormLabel>
-                <FormControl>
-                  <Input type="password" placeholder="Enter your password" autoComplete="on"{...field} />
-                </FormControl>
-                <FormDescription>If you have problem with your credentials please contact the media team.</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          >
-          </FormField>
-          <Button type="submit" disabled={form.formState.isSubmitting}>Login</Button>
-        </form>
-      </Form>
+      <div className="md:block hidden">
+        <Card>
+          <CardHeader>
+            <CardTitle>Account Login</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-4">
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>
+                        Email
+                      </FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter your email" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                >
+                </FormField>
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>
+                        Password
+                      </FormLabel>
+                      <FormControl>
+                        <Input type="password" placeholder="Enter your password" autoComplete="on" {...field} />
+                      </FormControl>
+                      <FormDescription>If you have problem with your credentials please contact the media team.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                >
+                </FormField>
+                <Button className="w-full" type="submit" disabled={form.formState.isSubmitting}>Login</Button>
+                <Link href="/">
+                  <Button className="w-full" variant="secondary"><ArrowLeft className="mr-2 h-4 w-4" />Back to Home</Button>
+                </Link>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="md:hidden">
+        <h3 className="text-2xl font-semibold leading-none tracking-tight mb-4">Account Login</h3>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    Email
+                  </FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            >
+            </FormField>
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    Password
+                  </FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="Enter your password" autoComplete="on" {...field} />
+                  </FormControl>
+                  <FormDescription>If you have problem with your credentials please contact the media team.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            >
+            </FormField>
+            <Button className="w-full" type="submit" disabled={form.formState.isSubmitting}>Login</Button>
+            <Link href="/">
+              <Button className="w-full" variant="secondary"><ArrowLeft className="mr-2 h-4 w-4" />Back to Home</Button>
+            </Link>
+          </form>
+        </Form>
+      </div>
       <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
         By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
         and <a href="#">Privacy Policy</a>.
