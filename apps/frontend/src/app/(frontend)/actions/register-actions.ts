71aaa63968d6c28dd97bc64a72eff1ccb3d1bdbd
Adamson Vispo <eydamson.dev@gmail.com>
2025-06-21 21:11:03 +0800

    chore: remove schema export and disable lint on the line

--- apps/frontend/src/app/(frontend)/actions/register-actions.ts
+++ apps/frontend/src/app/(frontend)/actions/register-actions.ts
@@ -5,7 +5,8 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
-export const RegisterSchema = z.object({
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const RegisterSchema = z.object({
   firstname: z.string().min(2, { message: 'First name must be at least 2 characters.' }),
   lastname: z.string().min(2, { message: 'Last name must be at least 2 characters.' }),
   email: z.string().email({ message: 'Invalid email address.' }),
